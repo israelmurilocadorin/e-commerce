5172c34d211eb2ed86e0532784bdd176afcbc505
israelmurilocadorin <israel.cadorin@catolicasc.org.br>
2020-06-08 20:11:14 -0300

    basedados

--- src/db_ecommerce.sql
+++ src/db_ecommerce.sql
@@ -3,22 +3,22 @@
 USE db_ecommerce;
 
 CREATE TABLE IF NOT EXISTS Clientes(
-    ID int NOT NULL AUTO_INCREMENT,
+    id_usuario INTEGER NOT NULL AUTO_INCREMENT,
     email VARCHAR(100) NOT NULL,
     senha VARCHAR(50) NOT NULL,
     nome VARCHAR(50) NOT NULL,
     cpf CHAR(11) NOT NULL,
     endereco VARCHAR(50) NOT NULL,
-    PRIMARY KEY (ID)
+    PRIMARY KEY (id_usuario)
 );
 
 CREATE TABLE IF NOT EXISTS Produtos(
-    ID int NOT NULL AUTO_INCREMENT,
+    id_produto INTEGER NOT NULL AUTO_INCREMENT,
     nome VARCHAR(200) NOT NULL,
     descricao TEXT,
     preco FLOAT NOT NULL,
     base64_foto_1 TEXT NOT NULL,
     base64_foto_2 TEXT NOT NULL,
     base64_foto_3 TEXT NOT NULL,
-    PRIMARY KEY (ID)
+    PRIMARY KEY (id_produto)
 );
